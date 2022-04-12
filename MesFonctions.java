f27a970511c92ef8d022531711eb3c2270f37fa7
frederic414 <freddipama@gmail.com>
2022-04-12 18:09:16 +0000

    Ajout de la fonction maximun et de la fonction factorielle

--- MesFonctions.java
+++ MesFonctions.java
@@ -1,19 +1,30 @@
 public class MesFonctions{
 
 
-public int minimum(int a, int b){
-	return Math.min(a,b);
+	public int minimum(int a, int b){
+		return Math.min(a,b);
 
-   }
-public boolean estPalindrome(String ch){
-	original=ch;
-	reverse="";
-	int taille=original.length();
-	for(int i=taille-1;i>=0;i--)
-		{
-		  reverse=reverse+original.charArt(i)
-		}
-	return  origin.equals(reverse);
-   }
+   	}
 
+	public int maximum(int a, int b){
+		return Math.max(a,b);
+
+	}
+	public boolean estPalindrome(String ch){
+		String original=ch;
+		String reverse="";
+		int taille=original.length();
+		for(int i=taille-1;i>=0;i--)
+			{
+		  		reverse=reverse+original.charArt(i);
+			}
+	return  original.equals(reverse);
+   	}
+
+	public int factorielle(int n){
+		if(n==0){
+		  return 1;
+		}else
+		  return (n*factorielle(n-1));
+	}
 }
