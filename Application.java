d41ca22c759b84318d6b1ebf77922be39d2a2113
frederic414 <freddipama@gmail.com>
2022-04-13 09:09:14 +0000

    correction de bug

--- Application.java
+++ Application.java
@@ -3,7 +3,7 @@ class Application{
  
  public static void main (String[] args){
        Scanner sc = new Scanner(System.in);
-        System.out.println("Choisisser la fonction\n 1 pour maxi  \n 2 pour mini \3 pour factorielle \4 pour palindrome");
+        System.out.println("Choisisser la fonction\n 1 pour maxi  \t 2 pour mini \t3 pour factorielle \t4 pour palindrome");
         int reponse = sc.nextInt();
         MesFonctions mf = new MesFonctions();
         int a, b = 0;
@@ -13,16 +13,19 @@ class Application{
                 a = sc.nextInt();
                 b = sc.nextInt();
                 System.out.println("Le maximum est:" + mf.maximum(a, b));
+		break;
             case 2:
                 System.out.println("Entrer les deux nombres ");
                 a = sc.nextInt();
                 b = sc.nextInt();
                 System.out.println("Le maximum est:" + mf.minimum(a, b));
+		break;
             case 3:
                 System.out.println("Entrer le nombre");
                 a = sc.nextInt();
 
                 System.out.println("Le factoriel est:" + mf.factorielle(a));
+		break;
             case 4:
                 System.out.println("Entrer le nombre/mot");
                 String ch = sc.nextLine();
@@ -31,6 +34,8 @@ class Application{
                 } else {
                     System.out.println("Le mot saisi n'est pas un palindrome");
                 }
+		break;
                 
    }
+}
 }
